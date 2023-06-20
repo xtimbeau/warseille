d562b8c577d255ecb841ce686a9a66338f73acdc
Jeanne DEVINEAU <jeanne.devineau@ensae.fr>
2023-06-20 14:46:19 +0000

    données DVF marseille

--- get_dv3f.R
+++ get_dv3f.R
@@ -8,6 +8,6 @@ con <- get_connection()
 db <- tbl(con,"DV3FV231_MUTATION") 
 
 local <- db |> 
-  dplyr::filter(CODDEP=="75") %>% 
+  dplyr::filter(CODDEP=="13") %>% 
   dplyr::select(IDMUTATION, DATEMUT, VALEURFONC) %>%
   collect()
