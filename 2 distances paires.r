6fb66a723e47a530918460b19a37f676a05bd554
Xavier Timbeau <xavier.timbeau@sciencespo.fr>
2023-10-28 21:39:01 +0000

    hey ho

--- 2 distances paires.r
+++ 2 distances paires.r
@@ -64,4 +64,50 @@ communes <- paires |> distinct(COMMUNE) |> collect() |> pull()
 paire <- paires |> 
   filter(COMMUNE == communes[[1]]) |> 
   collect()
-dodgr_pairs(od = paire |> slice(1:1000), routing = dodgr_router)
+dodgr_pairs(od = paire |> slice(1:5000), routing = dodgr_router)
+
+# deuxième essai, pairwise semble pas marcher
+
+mobpro <- qs::qread(mobpro_file) |> 
+  filter(mobpro99) |> 
+  group_by(COMMUNE, DCLT, TRANS) |> 
+  summarise(fl = any(fl),
+            fw = any(fw),
+            emp_scot = sum(NB_in)) |> 
+  pivot_wider(names_from = TRANS, values_from = emp_scot, values_fill = 0)
+
+iterator <- paires |> count(COMMUNE, DCLT) |> collect()
+
+logger::log_appender(logger::appender_file("logs/log_distances"))
+car_dist <- local({
+  pb <- progressr::progressor(steps = sum(iterator$n))
+  pb(0)
+  plan("multisession", workers = 4)
+  pmap_dfr(iterator, ~{
+    tictoc::tic()
+    les_paires <- paires |> 
+      filter(COMMUNE==.x, DCLT==.y) |> 
+      collect()
+    from <- les_paires |> 
+      distinct(fromidINS, .keep_all = TRUE) |> 
+      select(id = fromidINS, lon = o_lon, lat = o_lat) |> 
+      setDT()
+    to <- les_paires |> 
+      distinct(toidINS, .keep_all = TRUE) |> 
+      select(id = toidINS, lon = d_lon, lat = d_lat) |> 
+      setDT()
+    ttm <- dodgr_ttm(
+      o=from, 
+      d=to, 
+      tmax = Inf, 
+      routing = dodgr_router)
+    ss <- nrow(ttm$result)
+    pb(amount=ss)
+    time <- tictoc::toc(quiet=TRUE)
+    dtime <- (time$toc - time$tic)
+    speed_log <- stringr::str_c(
+      ofce::f2si2(ss), "@",ofce::f2si2(ss/dtime), "p/s")
+    logger::log_info(speed_log)
+    ttm$result[, `:=`(COMMUNE=.x, DCLT = .y)]
+  })
+})
