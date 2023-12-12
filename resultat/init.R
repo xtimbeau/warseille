292e3d003b629b91f830acca7fe9581494658d25
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2023-12-12 19:43:02 +0100

    update

---
+++ resultat/init.R
@@ -0,0 +1,30 @@
+library(knitr)
+library(sf)
+library(tidyverse)
+library(showtext)
+library(mapdeck)
+library(ofce)
+library(downloadthis)
+opts_chunk$set(
+  echo = FALSE,
+  warning = FALSE,
+  message = FALSE,
+  fig.pos="H", 
+  out.extra="",
+  dev="ragg_png",
+  fig.showtext=TRUE,
+  cache=FALSE)
+source("../secrets/azure.R")
+showtext_opts(dpi=200)
+showtext_auto()
+trim <- function(x, xm, xp) ifelse( x<= xm, xm, ifelse(x>= xp, xp, x))
+
+communes <- bd_read("communes")
+centre <- communes |> 
+  filter(INSEE_COM=="13019") |> 
+  st_transform(4326) |> 
+  st_centroid() |> 
+  st_coordinates()
+centre <- as.vector(centre)
+
+c200ze <- bd_read("c200ze") |> st_transform(4326)
