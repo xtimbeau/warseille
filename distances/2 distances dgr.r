51759ddeae984cf28ad343e2c503c5b5dec971e6
Xavier Timbeau <xavier.timbeau@sciencespo.fr>
2024-03-06 18:48:49 +0000

    flux plus bug distances

--- distances/2 distances dgr.r
+++ distances/2 distances dgr.r
@@ -42,9 +42,9 @@ DCLTs <- mobpro |> distinct(DCLT) |> pull()
 
 idINSes <- qs::qread(c200ze_file) |> 
   st_drop_geometry() |> 
-  select(com=com22, idINS, scot, emp_resident, ind) |> 
+  select(com, idINS, scot, emp_resident, ind) |> 
   mutate(from = scot & (ind>0) & com%in%COMMUNEs,
-         to = emp_resident>0 & com %in%DCLTs) |> 
+         to = emp_resident>0 & com%in%DCLTs) |> 
   filter(from | to)
 
 # voiture ---------
--- flux/meaps flux.R
+++ flux/meaps flux.R
@@ -33,16 +33,23 @@ rm <- qs::qread(rank_matrix)
 rt <- qs::qread(time_matrix)
 froms <- rownames(rm)
 tos <- colnames(rm)
+communes <- c200ze |> filter(scot) |> pull(com, name = idINS) 
+communes <- as.integer(communes[froms])
+dclts <- c200ze |> filter(emp_resident>0) |> pull(com, name = idINS) 
+dclts <- as.integer(dclts[tos])
+
 masses_AMP <- bd_read("AMP_masses")
+
+COMs <- tibble(actifs = masses_AMP$actifs, fuites = masses_AMP$fuites, from = froms, COMMUNE = communes )
+DCLTs <- tibble(emplois = masses_AMP$emplois, to = tos, DCLT = dclts)
 
 N <- length(masses_AMP$actifs)
 K <- length(masses_AMP$emplois)
 nb_tirages <- 16
-shufs <- matrix(NA, ncol = N, nrow = nb_tirages)
-for (i in 1:nb_tirages) shufs[i, ] <- sample(N, size = N)
+shufs <- emiette(les_actifs = masses_AMP$actifs, nshuf = 256, seuil = 300)
 modds <- matrix(1L, nrow=N, ncol=K)
 modds[is.na(rm)] <- NA
-modds[rd<10] <- 10
+modds[rt<10] <- 10
 dimnames(modds) <- dimnames(rm)
 
 tic();meaps <- meaps_multishuf(rkdist = rm, 
@@ -52,6 +59,43 @@ tic();meaps <- meaps_multishuf(rkdist = rm,
                 shuf = shufs, 
                 nthreads = 4, 
                 modds = modds); toc()
+
+# meaps <- qs::qread("{mdir}/tmp/meaps e 16t o1.qs" |> glue())
+
+meaps.c <- communaliser(meaps, communes, dclts)
+com_ar <- qs::qread(communes_ar_file)
+library(tmap)
+COMs |> 
+  group_by(COMMUNE) |> 
+  summarize(actifs = sum(actifs), fuites = sum(fuites)) |>
+  left_join(tibble(flux  = rowSums(meaps.c), COMMUNE = as.integer(names(rowSums(meaps.c)))), by = "COMMUNE") |> 
+  mutate(r = flux/actifs, COMMUNE = as.character(COMMUNE)) |> 
+  left_join(com_ar |> select(INSEE_COM), by = c('COMMUNE' = 'INSEE_COM')) |> 
+  st_as_sf() |> 
+  tm_shape() + tm_borders() + tm_fill(col="r")
+
+DCLTs |> 
+  mutate(DCLT = as.character(DCLT)) |> 
+  group_by(DCLT) |> 
+  summarize(emplois = sum(emplois)) |>
+  left_join(tibble(flux  = colSums(meaps.c), DCLT = names(colSums(meaps.c))), by = "DCLT") |> 
+  mutate(r = flux/emplois) |> 
+  left_join(com_ar |> select(INSEE_COM), by = c('DCLT' = 'INSEE_COM')) |> 
+  st_as_sf() |> 
+  filter(!st_is_empty(geometry)) |> 
+  tm_shape() + tm_borders() + tm_fill(col="r")
+
+tibble(from = froms, ai = matrixStats::rowSums2(meaps)) |> 
+  left_join(c200ze |> select(from = idINS, act_mobpro)) |>
+  mutate(r = ai/act_mobpro) |> 
+  st_as_sf() |> 
+  tm_shape() + tm_fill(col="r", style = "cont")
+tibble(to = tos, ej = matrixStats::colSums2(meaps)) |> 
+  left_join(c200ze |> select(to = idINS, emp_resident)) |>
+  mutate(r = ej/emp_resident) |> 
+  st_as_sf() |> 
+  tm_shape() + tm_fill(col="r", style = "cont")
+
 
 dimnames(meaps) <- dimnames(rm)
 meaps.dt <- as.data.table(meaps, keep.rownames = TRUE)
@@ -75,6 +119,5 @@ de_from <- de[,.(
   co2_pa = sum(co2_ij, na.rm=TRUE)/sum(f_ij),
   f_i = sum(f_ij, na.rm=TRUE)),
   by = c("mode","fromidINS", "meaps")] 
-
 
 kmco2_delta <- rbind(kmco2_delta, de_from) 
